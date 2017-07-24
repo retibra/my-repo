eb56b80f69831515ba0c4606bf4f23e99197dc65
Timothy Ege <retibra@gmail.com>
2017-07-24 20:54:38 +0000

    added attributes for lib

--- bpl-cli/lib/bpl/library.rb
+++ bpl-cli/lib/bpl/library.rb
@@ -1,6 +1,16 @@
+require 'nokogiri'
+require 'openUri'
+
 class BPL::Library
+
+  attr_accessor :name, :address, :contact_number, :hours
 
   def self.all
     puts "all libraries"
+    index_scrape = Nokogiri::HTML(open(http://www.bpl.org/general/hours/))
+
+
   end
+
+
 end
