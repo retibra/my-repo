e49dcc9f38e0194fef26591951dc9d96277ae12e
Timothy Ege <retibra@gmail.com>
2017-07-25 22:36:55 +0000

    hours almost working

--- bpl-cli/lib/bpl/library.rb
+++ bpl-cli/lib/bpl/library.rb
@@ -6,7 +6,7 @@ require 'pry'
 
 class BPL::Library
 
-  attr_accessor :name, :address, :contact_number, :hours
+  attr_accessor :name, :address, :zip_code, :contact_number, :hours
 
   @@all = []
 
@@ -21,13 +21,26 @@ class BPL::Library
 
   def self.scrape
     index_scrape = Nokogiri::HTML(open("http://www.bpl.org/general/hours/"))
+    days = ["Monday: ", "Tuesday: ", "Wednesday: ", "Thursday: ", "Friday: ", "Saturday: ", "Sunday: "]
+    totalhours = []
+    index_scrape.css("div#maincontent td").each_with_index do |element, i = 0|
+      if i.odd?
+         quickscrape = element.text.gsub("\u2013","")
+         hours = element.text.gsub("2013","").gsub("p.m.","p.m., ").gsub("a.m.","a.m. - ").split(',')
+         totalhours << days.zip(hours).map(&:join)
+         binding.pry
+      end
+
+    end
 
     index_scrape.css("div#maincontent h2").each_with_index do |element, i = 0|
       if i != 0
         lib = self.new
         lib.name = element.text
         lib.address = element.next.text.gsub(/617.*/,"")
+        lib.zip_code = lib.address[-5..-1]
         lib.contact_number = element.next.text.gsub(/.*617/,"617")[0..11]
+        lib.hours = index_scrape.css("div#maincontent td")
       end
     end
     binding.pry
